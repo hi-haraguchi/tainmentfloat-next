a3a2f8e6e43d1ef4571df09b6e806413ec676cc5
hi-haraguchi <hharaguchiroom8@gmail.com>
2025-09-14 23:55:59 +0900

    419エラー調整3（js-cookie）

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,13 +1,12 @@
 import Axios from 'axios'
+import Cookies from 'js-cookie'
 
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     withCredentials: true,
-    withXSRFToken: true,
-    xsrfCookieName: 'XSRF-TOKEN', // Laravelが返すクッキー名
-    xsrfHeaderName: 'X-XSRF-TOKEN', // Laravelが期待するヘッダー名
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        'X-XSRF-TOKEN': Cookies.get('XSRF-TOKEN'), // ✅ Cookie から直接読み込む
     },
 })
 
