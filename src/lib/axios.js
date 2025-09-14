b390d623ffa8d3b31d437747678dfb148f180e2b
hi-haraguchi <hharaguchiroom8@gmail.com>
2025-09-14 23:45:07 +0900

    419エラー調整2（ヘッダーに直接入れる）

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,11 +2,13 @@ import Axios from 'axios'
 
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    withCredentials: true,
+    withXSRFToken: true,
+    xsrfCookieName: 'XSRF-TOKEN', // Laravelが返すクッキー名
+    xsrfHeaderName: 'X-XSRF-TOKEN', // Laravelが期待するヘッダー名
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
     },
-    withCredentials: true,
-    withXSRFToken: true
 })
 
 export default axios
