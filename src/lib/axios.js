97c79afb3db857bd3e5fd82c7966caa63c5c4989
hi-haraguchi <hharaguchiroom8@gmail.com>
2025-09-15 10:35:05 +0900

    419エラー調整（Accept: 'application/json'）

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,25 +1,15 @@
 import Axios from 'axios'
 
-function getCookie(name) {
-    const value = `; ${document.cookie}`
-    const parts = value.split(`; ${name}=`)
-    if (parts.length === 2) return parts.pop().split(';').shift()
-}
-
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     withCredentials: true,
+    withXSRFToken: true, 
+    xsrfCookieName: 'XSRF-TOKEN', // ✅ Laravel が発行するCookie名
+    xsrfHeaderName: 'X-XSRF-TOKEN', // ✅ Laravel が期待するヘッダー名
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        Accept: 'application/json',
     },
-})
-
-axios.interceptors.request.use(config => {
-    const token = getCookie('XSRF-TOKEN')
-    if (token) {
-        config.headers['X-XSRF-TOKEN'] = decodeURIComponent(token) // ✅ デコードして送信
-    }
-    return config
 })
 
 export default axios
