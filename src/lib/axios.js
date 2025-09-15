4149df1fb73a62555b343bd210387cf0c2de02cc
hi-haraguchi <hharaguchiroom8@gmail.com>
2025-09-15 15:51:13 +0900

    axios にトークン再設定記述

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,10 +4,10 @@ const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
 
     // withCredentials: true,
-    // withXSRFToken: true, 
-    // xsrfCookieName: 'XSRF-TOKEN', 
+    // withXSRFToken: true,
+    // xsrfCookieName: 'XSRF-TOKEN',
     // Laravel が発行するCookie名
-    // xsrfHeaderName: 'X-XSRF-TOKEN', 
+    // xsrfHeaderName: 'X-XSRF-TOKEN',
     // Laravel が期待するヘッダー名
 
     headers: {
@@ -15,5 +15,14 @@ const axios = Axios.create({
         Accept: 'application/json',
     },
 })
+
+
+// ページ読み込み時に localStorage のトークンを再設定
+if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('auth_token')
+    if (token) {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    }
+}
 
 export default axios
