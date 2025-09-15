5affc411265296070157f011e33e19d1d3fb70cc
hi-haraguchi <hharaguchiroom8@gmail.com>
2025-09-15 10:06:25 +0900

    419エラー調整（xsrfHeaderName: 'X-XSRF-TOKEN',）

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,21 +1,14 @@
 import Axios from 'axios'
-import Cookies from 'js-cookie'
 
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     withCredentials: true,
+    withXSRFToken: true, 
+    xsrfCookieName: 'XSRF-TOKEN', // Laravel が発行する Cookie 名
+    xsrfHeaderName: 'X-XSRF-TOKEN', // Laravel が期待するヘッダー名
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
     },
-})
-
-// リクエストごとに XSRF-TOKEN を動的にセット
-axios.interceptors.request.use(config => {
-    const token = Cookies.get('XSRF-TOKEN')
-    if (token) {
-        config.headers['X-XSRF-TOKEN'] = token
-    }
-    return config
 })
 
 export default axios
