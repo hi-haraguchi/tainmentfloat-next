93e74ad6a0b18910717181b35c3d1071b1cb919a
hi-haraguchi <hharaguchiroom8@gmail.com>
2025-09-23 08:08:56 +0900

    アイコンなどとりあえず

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,20 @@ const RootLayout = ({ children }) => {
 }
 
 export const metadata = {
-    title: 'Laravel',
+    title: 'エンタメフロート',
+    openGraph: {
+        title: 'エンタメフロート',
+        description: '社会とちょうどいい距離を保つエンタメSNS',
+        images: ['/tf-rogo-p22.png'],
+    },
+    icons: {
+        icon: [
+            { url: '/tf-favicon.ico', sizes: 'any' },
+            { url: '/tf-favicon.svg', type: 'image/svg+xml' },
+            { url: '/tf-favicon.png', type: 'image/png' },
+        ],
+        apple: '/apple-touch-icon.png',
+    },
 }
 
 export default RootLayout
